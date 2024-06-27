f1e4a6f883ce74e8deae2048dcc096202b337edc
Joachim Løvgaard <joachim@loevgaard.dk>
2024-06-27 13:11:09 +0200

    Add missing methods to interface

--- src/Client/Endpoint/ProductEndpointInterface.php
+++ src/Client/Endpoint/ProductEndpointInterface.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Setono\PeakWMS\Client\Endpoint;
 
 use Setono\PeakWMS\DataTransferObject\Collection;
+use Setono\PeakWMS\DataTransferObject\PaginatedCollection;
 use Setono\PeakWMS\DataTransferObject\Product\Product;
+use Setono\PeakWMS\Request\Query\Product\PageQuery;
 
 /**
  * @extends EndpointInterface<Product>
@@ -14,7 +16,17 @@ use Setono\PeakWMS\DataTransferObject\Product\Product;
 interface ProductEndpointInterface extends EndpointInterface, CreatableEndpointInterface, DeletableEndpointInterface
 {
     /**
+     * @return PaginatedCollection<Product>
+     */
+    public function getPage(PageQuery $query): PaginatedCollection;
+
+    /**
      * @return Collection<Product>
      */
     public function getByProductId(string $productId): Collection;
+
+    /**
+     * @return iterable<Product>
+     */
+    public function iterate(PageQuery $query = null): iterable;
 }
